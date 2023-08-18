46d3c6afd2747cd67b971def09d6aaa03409aac5
Chesta Patel <chestapatel123@gmail.com>
2023-08-17 22:39:06 -0400

    Added Android project files

---
+++ MDEV1004M2023ASSIGNMENT4ANDROID/app/src/main/java/ca/georgiancollege/mdev1004_m2023_assignment4_android/MainActivity.java
@@ -0,0 +1,16 @@
+package ca.georgiancollege.mdev1004_m2023_assignment4_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
