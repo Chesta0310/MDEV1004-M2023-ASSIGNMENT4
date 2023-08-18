0089cfc15f1c21e51e14f37816f408e5d7fffca0
Chesta Patel <chestapatel123@gmail.com>
2023-08-17 22:25:30 -0400

    Added user controller and endpoints

--- MDEV1004-M2023-ASSIGNMENT4-BACKEND/Server/Routes/index.js
+++ MDEV1004-M2023-ASSIGNMENT4-BACKEND/Server/Routes/index.js
@@ -6,20 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 let router = express_1.default.Router();
 const movie_1 = require("../Controllers/movie");
-router.get("/list", (req, res, next) => {
-    (0, movie_1.DisplayMovieList)(req, res, next);
-});
-router.get("/find/:id", (req, res, next) => {
-    (0, movie_1.DisplayMovieByID)(req, res, next);
-});
-router.post("/add", (req, res, next) => {
-    (0, movie_1.AddMovie)(req, res, next);
-});
-router.put("/update/:id", (req, res, next) => {
-    (0, movie_1.UpdateMovie)(req, res, next);
-});
-router.delete("/:id", (req, res, next) => {
-    (0, movie_1.DeleteMovie)(req, res, next);
-});
+const user_1 = require("../Controllers/user");
+router.get("/list", (req, res, next) => (0, movie_1.DisplayMovieList)(req, res, next));
+router.get("/find/:id", (req, res, next) => (0, movie_1.DisplayMovieByID)(req, res, next));
+router.post("/add", (req, res, next) => (0, movie_1.AddMovie)(req, res, next));
+router.put("/update/:id", (req, res, next) => (0, movie_1.UpdateMovie)(req, res, next));
+router.delete("/:id", (req, res, next) => (0, movie_1.DeleteMovie)(req, res, next));
+router.post("/register", (req, res, next) => (0, user_1.ProcessRegisterPage)(req, res, next));
+router.post("/login", (req, res, next) => (0, user_1.ProcessLogin)(req, res, next));
+router.get("/logout", (req, res, next) => (0, user_1.ProcessLogout)(req, res, next));
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- MDEV1004-M2023-ASSIGNMENT4-BACKEND/Server/Routes/index.ts
+++ MDEV1004-M2023-ASSIGNMENT4-BACKEND/Server/Routes/index.ts
@@ -6,32 +6,34 @@
 import express from "express";
 let router = express.Router();
 
-/* Get the movie Controller */
+/* Get the movie controller functions */
 import { DisplayMovieList, DisplayMovieByID, AddMovie, UpdateMovie, DeleteMovie } from "../Controllers/movie";
 
+/* Get the auth controller functions */
+import { ProcessLogin, ProcessLogout, ProcessRegisterPage } from "../Controllers/user";
+
 /* GET /api/list - display the movie list */
-router.get("/list", (req, res, next) => {
-    DisplayMovieList(req, res, next);
-});
+router.get("/list", (req, res, next) => DisplayMovieList(req, res, next));
 
 /* GET /api/find/:id - display a movie by id */
-router.get("/find/:id", (req, res, next) => {
-    DisplayMovieByID(req, res, next);
-});
+router.get("/find/:id", (req, res, next) => DisplayMovieByID(req, res, next));
 
 /* POST /api/add - add a new movie */
-router.post("/add", (req, res, next) => {
-    AddMovie(req, res, next);
-});
+router.post("/add", (req, res, next) => AddMovie(req, res, next));
 
 /* PUT /api/update/:id - update a movie by id */
-router.put("/update/:id", (req, res, next) => {
-    UpdateMovie(req, res, next);
-});
+router.put("/update/:id", (req, res, next) => UpdateMovie(req, res, next));
 
 /* GET /api/delete/:id - delete a movie by id */
-router.delete("/:id", (req, res, next) => {
-    DeleteMovie(req, res, next);
-});
+router.delete("/:id", (req, res, next) => DeleteMovie(req, res, next));
+
+/* POST /api/register - add a new user */
+router.post("/register", (req, res, next) => ProcessRegisterPage(req, res, next));
+
+/* POST /api/login - login a user */
+router.post("/login", (req, res, next) => ProcessLogin(req, res, next));
+
+/* GET /api/logout - logout a user */
+router.get("/logout", (req, res, next) => ProcessLogout(req, res, next));
 
 export default router;
